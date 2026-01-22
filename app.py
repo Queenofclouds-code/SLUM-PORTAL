9afe844c2c8a88ebea788bbc829cab1a19159e5c
Queenofclouds-code <meghajk774@gmail.com>
2026-01-22 16:36:35 +0530

    Final push: completes project files

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+import config
 from flask import Flask
-from model import db
 from routes import slum_routes
+from model import db
 
-app = Flask(
-    __name__,
-    template_folder="Frontend"
-)
+# Initialize Flask app with the template and static folder
+app = Flask(__name__)
 
-app.config.from_pyfile("config.py")
+# Load the configuration
+app.config.from_object(config)
 
+# Initialize the database
 db.init_app(app)
 
-app.register_blueprint(slum_routes)
+# Register the routes blueprint
+app.register_blueprint(slum_routes, url_prefix="/slum-portal/api")
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
